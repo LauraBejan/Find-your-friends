6d27a9489fa7039ac1d7e4426189d0b58735b440
Laura <laura@Lauras-Mac.local>
2018-08-16 10:13:39 -0700

    Added localization

--- ex2/ex2/ViewController.swift
+++ ex2/ex2/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import MapKit
 import CoreData
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -17,12 +18,64 @@ class ViewController: UIViewController {
     @IBOutlet weak var fixedLastUpdate: UILabel!
     @IBOutlet weak var lastUpdateCoreData: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     
     }
+    
+    @IBAction func goToEnglish(_ sender: Any) {
+        
+        if Bundle.main.preferredLocalizations.first != "en" {
+            
+            let confirmAlertCtrl = UIAlertController(title: NSLocalizedString("Fermer l'application pour appliquer les modifications", comment: ""), message: NSLocalizedString("Relancez manuellement", comment: ""), preferredStyle: .alert)
+            
+            let confirmAction = UIAlertAction(title: NSLocalizedString("Fermer", comment: ""), style: .destructive)
+            {_ in UserDefaults.standard.set(["en"], forKey: "AppleLanguages")
+                UserDefaults.standard.synchronize()
+                exit(EXIT_SUCCESS)
+            }
+            
+            confirmAlertCtrl.addAction(confirmAction)
+            
+            let cancelAction = UIAlertAction(title: NSLocalizedString("Anuller", comment: ""), style: . cancel, handler: nil)
+            
+            confirmAlertCtrl.addAction(cancelAction)
+            
+            present(confirmAlertCtrl, animated: true, completion: nil)
+            
+            
+        }
 
+    }
+    
+    @IBAction func goToFrench(_ sender: Any) {
+        
+        if Bundle.main.preferredLocalizations.first != "fr" {
+            
+            let confirmAlertCtrl = UIAlertController(title: NSLocalizedString("Close the app to apply the changes", comment: ""), message: NSLocalizedString("Relaunch manually", comment: ""), preferredStyle: .alert)
+            
+            let confirmAction = UIAlertAction(title: NSLocalizedString("Close", comment: ""), style: .destructive)
+            {_ in UserDefaults.standard.set(["fr"], forKey: "AppleLanguages")
+                UserDefaults.standard.synchronize()
+                exit(EXIT_SUCCESS)
+            }
+            
+            confirmAlertCtrl.addAction(confirmAction)
+            
+            let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: . cancel, handler: nil)
+            
+            confirmAlertCtrl.addAction(cancelAction)
+            
+            present(confirmAlertCtrl, animated: true, completion: nil)
+            
+            
+        }
+        
+    
+    }
+    
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
